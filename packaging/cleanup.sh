77d7a9ef19e39faf1b21bee9edf2af4cda26968d
Justas Balcas <justas.balcas@cern.ch>
2015-05-07 13:56:19 +0200

    Clean up script for old data

---
+++ packaging/cleanup.sh
@@ -0,0 +1,9 @@
+#Clean up production data which was not updated longer then 1 day
+sudo find /dataprod/ -maxdepth 1 -type d -mtime +1 -exec rm -rf {} \;
+#Clean up analysis data which was not updated longer then 1 day
+sudo find /dataana/ -maxdepth 1 -type d -mtime +1 -exec rm -rf {} \;
+
+#Clean up tmp 
+for x in {a..z}; do rm -f /tmp/tmp$x*.png; done
+for x in {A..Z}; do rm -f /tmp/tmp$x*.png; done
+for x in {0..9}; do rm -f /tmp/tmp$x*.png; done
