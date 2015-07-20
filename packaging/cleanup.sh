809f43f603b3ddfe18fec61b91ce9e6ca004f904
Justas Balcas <justas.balcas@cern.ch>
2015-07-20 07:35:52 +0200

    Update cleanup, fix date in total view. Change colors

--- packaging/cleanup.sh
+++ packaging/cleanup.sh
@@ -2,8 +2,7 @@
 find /dataprod/ -maxdepth 1 -type d -mtime +1 -exec rm -rf {} \;
 #Clean up analysis data which was not updated longer then 1 day
 find /dataana/ -maxdepth 1 -type d -mtime +1 -exec rm -rf {} \;
+#Clean up analysis crab2 data which was not updated longer then 1 day
+find /datacrab2/ -maxdepth 1 -type d -mtime +1 -exec rm -rf {} \;
 
-#Clean up tmp 
-for x in {a..z}; do rm -f /tmp/tmp$x*.png; done
-for x in {A..Z}; do rm -f /tmp/tmp$x*.png; done
-for x in {0..9}; do rm -f /tmp/tmp$x*.png; done
+
