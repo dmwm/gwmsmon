4e1cc1d4fb6de28edc89e6e8d0ab54ec09fa7820
Justas Balcas <justas.balcas@cern.ch>
2016-05-20 10:24:15 +0200

    Clean summary files older than 30mins

--- packaging/cleanup.sh
+++ packaging/cleanup.sh
@@ -6,3 +6,7 @@ find /idata_crab3/ -maxdepth 2 -type d -mtime +7 -exec rm -rf {} \;
 find /data_crab2/ -maxdepth 1 -type d -mtime +1 -exec rm -rf {} \;
 
 
+# Remove summary files which were not updated longer than 1d.
+find /idata_prod/ -maxdepth 2 -mmin +30 -type f -name summary.json -exec rm -rf {} \;
+find /idata_crab3/ -maxdepth 2 -mmin +30 -type f -name summary.json -exec rm -rf {} \;
+
